654f18ddf0c6f29b9f5958b3c82a43c67a9a68b4
sarika1999 <sarika1999@gmail.com>
2026-01-19 21:23:32 -0500

    code

--- misc/spatial_trends.R
+++ misc/spatial_trends.R
@@ -5,43 +5,38 @@ library(sf)
 library(urbnmapr)
 library(viridis)
 
-# Example: if your data is in a CSV
-# It should have columns like: fips, flood_count
-floods <- read_csv("~/Desktop/panic/misc/flood_counts.csv")
-floods <- floods %>%
-  mutate(fips = str_pad(fips, width = 5, pad = "0"))
+setwd('./multinomial_GP')
 
-# Example structure if already in memory:
-# floods <- tibble(
-#   fips = c("06037", "06059", "48113"),  # LA, Orange (CA), Dallas (TX)
-#   flood_count = c(5, 3, 10)
-# )
+dat <- readRDS('data.rds') #read in data 
+
+dat <- dat %>% select(floodcty_id, duration) %>% unique() %>% mutate(fips = substr(floodcty_id, nchar(floodcty_id) - 4, nchar(floodcty_id)))
+
+summary_stats <- dat %>% group_by(fips) %>% summarise(avg = mean(duration), counts = n())
+
+summary_stats <- summary_stats %>%
+  mutate(fips = str_pad(fips, width = 5, pad = "0"))
 
 # Load county geometries (from Urban Institute)
 counties <- get_urbn_map("counties", sf = TRUE)
 states <- get_urbn_map("states", sf = TRUE)
 
-# Inspect to confirm FIPS column name
-# names(counties)
-# -> includes "county_fips"
-
 # Join using FIPS code
 county_floods_map <- counties %>%
-  left_join(floods, by = c("county_fips" = "fips"))
+  left_join(summary_stats, by = c("county_fips" = "fips"))
 
-# Filter out Alaska and Hawaii
+# Remove Alaska and Hawaii
 county_floods_map <- county_floods_map %>%
   filter(!state_name %in% c("Alaska", "Hawaii"))
 
 states <- states %>%
   filter(!state_name %in% c("Alaska", "Hawaii"))
 
-# Plot with borders visible (even for NA)
-ggplot() +
-  geom_sf(data = county_floods_map, aes(fill = frequency), color = "white", size = 0.1) +  # county borders
-  geom_sf(data = states, fill = NA, color = "black", size = 0.4) +  # state outlines
+# Frequency 
+freq <- ggplot() +
+  geom_sf(data = county_floods_map, aes(fill = counts), color = "white", size = 0.1) +  # county borders
+  geom_sf(data = states, fill = NA, color = "black", size = 0.5) +  # state outlines
   scale_fill_viridis(
-    option = "plasma",
+    option = "magma",
     name = "Frequency",
     na.value = "grey90"
   ) +
@@ -49,11 +44,36 @@ ggplot() +
   theme(
     axis.text = element_blank(),
     axis.ticks = element_blank(),
-    panel.grid = element_blank()
+    panel.grid = element_blank(),
+    legend.title = element_text(size = 12, face = "bold"),
+    legend.text = element_text(size = 10)
   )
 
 county_floods_map %>%
   group_by(state_name) %>%
-  summarise(has_data = any(!is.na(frequency))) %>%
+  summarise(has_data = any(!is.na(counts))) %>%
   filter(!has_data) %>%
   pull(state_name)
+
+# Average duration
+dur <- ggplot() +
+  geom_sf(data = county_floods_map, aes(fill = avg), color = "white", size = 0.1) +  # county borders
+  geom_sf(data = states, fill = NA, color = "black", size = 0.5) +  # state outlines
+  scale_fill_viridis(
+    option = "magma",
+    name = "Average duration",
+    breaks = seq(0, 10, by = 2),
+    limits = c(0, 10),
+    na.value = "grey90"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    panel.grid = element_blank(),
+    legend.title = element_text(size = 12, face = "bold"),
+    legend.text = element_text(size = 10)
+  )
+
+library(ggpubr)
+ggarrange(freq, dur, labels = c("A", "B"), ncol = 2)
