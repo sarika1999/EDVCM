0cada0385484a1aa55891c9892f18b2f928221ef
sarika1999 <sarika1999@gmail.com>
2026-08-05 11:06:02 -0500

    fix

--- code/application/setup/cov_spline_setup.R
+++ code/application/setup/cov_spline_setup.R
@@ -1,4 +1,4 @@
-medicare_cause_2000_2016_covariates_dur10 <- readRDS("./multinomial_GP/data/real_data/county/FFS/medicare_cause_2000_2016_covariates_dur10.rds")
+covariates_dur10 <- readRDS("./multinomial_GP/FFS_final_oct2025/covariates_dur10.rds")
 
 library(Hmisc)
 
@@ -6,12 +6,12 @@ get_spline <- function(x) {
   rcspline.eval(x, nk = 5, inclx = FALSE)
 }
 
-tmmx <- get_spline(medicare_cause_2000_2016_covariates_dur10$tmmx)
-rmax <- get_spline(medicare_cause_2000_2016_covariates_dur10$rmax)
-vs <- get_spline(medicare_cause_2000_2016_covariates_dur10$vs)
-pm25 <- get_spline(medicare_cause_2000_2016_covariates_dur10$pm25)
-o3 <- get_spline(medicare_cause_2000_2016_covariates_dur10$o3)
-no2 <- get_spline(medicare_cause_2000_2016_covariates_dur10$no2)
+tmmx <- get_spline(covariates_dur10$tmmx)
+rmax <- get_spline(covariates_dur10$rmax)
+vs <- get_spline(covariates_dur10$vs)
+pm25 <- get_spline(covariates_dur10$pm25)
+o3 <- get_spline(covariates_dur10$o3)
+no2 <- get_spline(covariates_dur10$no2)
 
 conf <- cbind(tmmx, rmax, vs, pm25, o3, no2)
-saveRDS(conf, './multinomial_GP/data/real_data/county/FFS_final_oct2025/spline_covariates_dur10.rds')
+saveRDS(conf, './multinomial_GP/FFS_final_oct2025/spline_covariates_dur10.rds')
